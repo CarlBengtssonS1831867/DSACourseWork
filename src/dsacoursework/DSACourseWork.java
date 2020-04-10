12047397dc55a41c8ec4cc22726cfad2b42c7278
Carl Bengtsson <Carl Bengtsson@DESKTOP-635VJ6N>
2020-04-10 07:58:53 +0100

    DSA Course Work

---
+++ src/dsacoursework/DSACourseWork.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsacoursework;
+
+/**
+ *
+ * @author Carl Bengtsson
+ */
+public class DSACourseWork {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
